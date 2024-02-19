f6aeb09a4f4d0295d465bcd4b51c12b631e4847a
stephenbowyer <github@ste.widearea.org>
2024-02-19 16:47:24 +0000

    5-GET /api/articles with comments, sorting and additional tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -122,7 +122,35 @@ describe('GET /api/articles', () => {
             .then(({body}) => {
                 expect(body.articles.length).toBe(data.articleData.length);
                 expect(typeof body.articles[0]).toBe('object');
-                expect(Object.keys(body.articles[0])).toContain('author', 'title', 'article_id', 'body', 'topic', 'created_at', 'votes', 'article_img_url');
+                expect(Object.keys(body.articles[0])).toContain('author', 'title', 'article_id', 'topic', 'created_at', 'votes', 'article_img_url', 'comment_count');
+                expect(Object.keys(body.articles[0])).not.toContain('body');
+            });
+    });
+    test('200: should return same number of articles as in dataset', () => {
+        return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body}) => {
+                expect(body.articles.length).toBe(data.articleData.length);
+            });
+    });
+    test('200: should return articles sorted by created date', () => {
+        return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body}) => {
+                expect(body.articles).toBeSortedBy('created_at', {descending: true});
+            });
+    });
+    test('200: should return correct count of comments for each article', () => {
+        return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body}) => {
+                body.articles.forEach((article) => {
+                    const expectedLength = data.commentData.filter((comment) => comment.article_id === article.article_id).length;
+                    expect(Number(article.comment_count)).toBe(expectedLength);
+                });
             });
     });
 });
--- models/articles.models.js
+++ models/articles.models.js
@@ -13,7 +13,17 @@ function selectArticle(articleId){
 }
 
 function selectAllArticles(){
-    const queryString = format('SELECT * FROM ARTICLES');
+    const queryString = `
+        SELECT articles.article_id, articles.title, articles.topic,
+            articles.author, articles.created_at,
+            articles.votes, articles.article_img_url,
+            COUNT(comments.body) AS comment_count
+        FROM articles
+        LEFT JOIN comments
+            ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC
+    `;
     return db.query(queryString).then((result) => {
         if (result.rowCount === 0)
             return Promise.reject({status: 404, msg: "Not Found"});
