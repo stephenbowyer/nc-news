3626916632cde2760a45160314d21a2bcae77aef
stephenbowyer <github@ste.widearea.org>
2024-02-19 12:28:33 +0000

    3-GET /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app.js');
 const db = require('../db/connection.js');
 const seed = require('../db/seeds/seed.js');
-const data = require('../db/data/test-data/index.js')
+const data = require('../db/data/test-data/index.js');
 
 beforeEach(() => {
     return seed(data);
@@ -44,6 +44,30 @@ describe('GET /api/topics', () => {
             .then(({body}) => {
                 expect(typeof body.topics[0].slug).toBe('string');
                 expect(typeof body.topics[0].description).toBe('string');
+            });
+    });
+});
+describe('GET /api', () => {
+    test('200: should return an object containing the description for this endpoint', () => {
+        return request(app)
+            .get('/api')
+            .expect(200)
+            .then(({body}) => {
+                expect(typeof body.endpoints).toBe('object');
+                expect(Object.keys(body.endpoints['GET /api'])).toContain('description');
+                expect(typeof body.endpoints['GET /api'].description).toBe('string');
+            });
+    });
+    test('200: should return an object containing the description, valid queries and example response for /api/topics endpoint', () => {
+        return request(app)
+            .get('/api')
+            .expect(200)
+            .then(({body}) => {
+                expect(typeof body.endpoints).toBe('object');
+                expect(Object.keys(body.endpoints['GET /api/topics'])).toContain('description', 'queries', 'exampleResponse');
+                expect(typeof body.endpoints['GET /api/topics'].description).toBe('string');
+                expect(Array.isArray(body.endpoints['GET /api/topics'].queries)).toBe(true);
+                expect(body.endpoints['GET /api/topics'].queries.length).toBe(0);
             });
     });
 });
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const {getTopics} = require('./controllers/topics.controllers.js');
+const {getApis} = require('./controllers/api.controllers.js');
 
 app.get('/api/topics', getTopics);
+app.get('/api', getApis);
 
 module.exports = app;
