5237a8e8a5d84cdc7d8136a223c39a7e878eed67
stephenbowyer <github@ste.widearea.org>
2024-03-14 15:34:18 +0000

    Added correct api route for comment voting

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,6 +1,7 @@
 const commentsRouter = require('express').Router();
-const {deleteComment} = require('../controllers/comments.controllers.js');
+const {deleteComment, patchComment} = require('../controllers/comments.controllers.js');
 
 commentsRouter.delete('/:comment_id', deleteComment);
+commentsRouter.patch('/:comment_id', patchComment);
 
 module.exports = commentsRouter;
